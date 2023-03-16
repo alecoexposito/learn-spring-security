dd987c09b62de263948763ac2a1cdb7fb28037ee
Alejandro Exposito Alvarez <aalvarez@quickbase.com>
2023-03-16 19:40:41 +0200

    Changes in java code

--- m1-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,45 +1,32 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+public class LssSecurityConfig {
 
-    public LssSecurityConfig() {
-        super();
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
     }
 
-    //
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests().anyRequest().authenticated()
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
-        auth.
-            inMemoryAuthentication().
-            withUser("user").password("pass").
-            roles("USER");
-    } // @formatter:on
+                .and().formLogin().loginPage("/login").permitAll().loginProcessingUrl("/doLogin")
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
-        http
-        .authorizeRequests()
-                .anyRequest().authenticated()
-        
-        .and()
-        .formLogin().
-            loginPage("/login").permitAll().
-            loginProcessingUrl("/doLogin")
+                .and().logout().permitAll().logoutUrl("/doLogout")
 
-        .and()
-        .logout().permitAll().logoutUrl("/doLogout")
-        
-        .and()
-        .csrf().disable()
-        ;
+                .and().csrf().disable().build();
     }
-
 }
